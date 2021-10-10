c883c23239519d71107c4c849fc5684348b82431
TylerGoeringer <tyler.goeringer@gmail.com>
2021-10-10 13:48:29 -0700

    Minor tweaks + local deployment batch script

--- copy_assemblies.bat
+++ copy_assemblies.bat
@@ -1,3 +1,2 @@
-ECHO "Copying DLLs from %WRATH_DIR%\Wrath_Data\Managed\ to %cd%\Assemblies\"
 XCOPY "%WRATH_DIR%\Wrath_Data\Managed\*.dll" "%cd%\Assemblies" /Y /I
 XCOPY "%WRATH_DIR%\Wrath_Data\Managed\UnityModManager\UnityModManager.dll" "%cd%\Assemblies" /Y /I
---
+++ deploy.bat
@@ -0,0 +1,2 @@
+XCOPY "%cd%\Blueprint-Core\bin\Debug\net4.7.1\Blueprint-Core.dll" "%WRATH_DIR%\Mods\BlueprintCore" /Y /I
+XCOPY "%cd%\Blueprint-Core\bin\Debug\net4.7.1\Info.json" "%WRATH_DIR%\Mods\BlueprintCore" /Y /I
