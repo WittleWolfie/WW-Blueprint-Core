ef1598f7034f9ffcf03669ad773c67ebaa95946c
TylerGoeringer <tyler.goeringer@gmail.com>
2021-10-15 14:40:52 -0700

    Expose library Version in Main
    This might be useful for optional feature support and might be necessary for non-UMM based mods.

--- Blueprint-Core/Main.cs
+++ Blueprint-Core/Main.cs
@@ -1,12 +1,20 @@
+using System;
 using HarmonyLib;
 using UnityModManagerNet;
 
 namespace BlueprintCore
 {
-  static class Main
+  public static class Main
   {
+    /**
+     * Returns the current version of the mod. You can check this to determine if specific feature
+     * support is available.
+     */
+    public static Version Version { get; private set; }
+
     static bool Load(UnityModManager.ModEntry modEntry)
     {
+      Version = modEntry.Version;
       var harmony = new Harmony(modEntry.Info.Id);
       harmony.PatchAll();
       return true;
