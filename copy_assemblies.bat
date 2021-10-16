815518b72a9c4f849e9b561acef0ae1ca58d3310
TylerGoeringer <tyler.goeringer@gmail.com>
2021-10-16 11:52:06 -0700

    Convert from batch files to MSBuild & add Moq

--- copy_assemblies.bat
+++
@@ -1,2 +0,0 @@
-XCOPY "%WRATH_DIR%\Wrath_Data\Managed\*.dll" "%cd%\Assemblies" /Y /I
-XCOPY "%WRATH_DIR%\Wrath_Data\Managed\UnityModManager\UnityModManager.dll" "%cd%\Assemblies" /Y /I
--- deploy.bat
+++
@@ -1,2 +0,0 @@
-XCOPY "%cd%\Blueprint-Core\bin\Debug\net4.7.1\Blueprint-Core.dll" "%WRATH_DIR%\Mods\BlueprintCore" /Y /I
-XCOPY "%cd%\Blueprint-Core\bin\Debug\net4.7.1\Info.json" "%WRATH_DIR%\Mods\BlueprintCore" /Y /I
