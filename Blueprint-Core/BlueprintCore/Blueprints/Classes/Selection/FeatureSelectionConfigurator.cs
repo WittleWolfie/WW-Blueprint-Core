28e9baaad2600bd2ac40690fc8b6781a19190ddd
TylerGoeringer <tyler.goeringer@gmail.com>
2021-10-24 15:08:16 -0700

    Add BlueprintFeatureSelection fields

--- Blueprint-Core/BlueprintCore/Blueprints/Classes/Selection/FeatureSelectionConfigurator.cs
+++ Blueprint-Core/BlueprintCore/Blueprints/Classes/Selection/FeatureSelectionConfigurator.cs
@@ -1,7 +1,10 @@
 using BlueprintCore.Utils;
 using Kingmaker.Blueprints;
+using Kingmaker.Blueprints.Classes;
 using Kingmaker.Blueprints.Classes.Prerequisites;
 using Kingmaker.Blueprints.Classes.Selection;
+using System;
+using System.Linq;
 
 namespace BlueprintCore.Blueprints.Classes.Selection
 {
@@ -30,6 +33,70 @@ namespace BlueprintCore.Blueprints.Classes.Selection
     {
       BlueprintTool.Create<BlueprintFeatureSelection>(name, assetId);
       return For(name);
+    }
+
+    /// <summary>
+    /// Sets <see cref="BlueprintFeatureSelection.IgnorePrerequisites"/>
+    /// </summary>
+    public FeatureSelectionConfigurator SetIgnorePrerequisites(bool ignore = true)
+    {
+      return OnConfigureInternal(blueprint => blueprint.IgnorePrerequisites = ignore);
+    }
+
+    /// <summary>
+    /// Sets <see cref="BlueprintFeatureSelection.Mode"/>
+    /// </summary>
+    public FeatureSelectionConfigurator SetMode(SelectionMode mode)
+    {
+      return OnConfigureInternal(blueprint => blueprint.Mode = mode);
+    }
+
+    /// <summary>
+    /// Sets <see cref="BlueprintFeatureSelection.Group"/>
+    /// </summary>
+    public FeatureSelectionConfigurator SetPrimaryGroup(FeatureGroup group)
+    {
+      return OnConfigureInternal(blueprint => blueprint.Group = group);
+    }
+
+    /// <summary>
+    /// Sets <see cref="BlueprintFeatureSelection.Group2"/>
+    /// </summary>
+    public FeatureSelectionConfigurator SetSecondaryGroup(FeatureGroup group)
+    {
+      return OnConfigureInternal(blueprint => blueprint.Group2 = group);
+    }
+
+    /// <summary>
+    /// Adds to <see cref="BlueprintFeatureSelection.m_AllFeatures"/>
+    /// </summary>
+    /// 
+    /// <param name="features"><see cref="BlueprintFeature"/></param>
+    public FeatureSelectionConfigurator AddFeatures(params string[] features)
+    {
+      return OnConfigureInternal(
+          blueprint =>
+          {
+            blueprint.m_AllFeatures =
+                CommonTool.Append(
+                    blueprint.m_AllFeatures,
+                    features.Select(feature => BlueprintTool.GetRef<BlueprintFeatureReference>(feature)).ToArray());
+          });
+    }
+
+    /// <summary>
+    /// Removes from <see cref="BlueprintFeatureSelection.m_AllFeatures"/>
+    /// </summary>
+    /// 
+    /// <param name="features"><see cref="BlueprintFeature"/></param>
+    public FeatureSelectionConfigurator RemoveFeatures(params string[] features)
+    {
+      return OnConfigureInternal(
+          blueprint =>
+          {
+            var featureRefs = features.Select(feature => BlueprintTool.GetRef<BlueprintFeatureReference>(feature));
+            blueprint.m_AllFeatures = blueprint.m_AllFeatures.Except(featureRefs).ToArray();
+          });
     }
 
     /// <summary>
--- Tests/Blueprints/Classes/Selection/FeatureSelectionConfiguratorTest.cs
+++ Tests/Blueprints/Classes/Selection/FeatureSelectionConfiguratorTest.cs
@@ -1,9 +1,11 @@
 using BlueprintCore.Blueprints;
 using BlueprintCore.Blueprints.Classes.Selection;
 using Kingmaker.Blueprints;
+using Kingmaker.Blueprints.Classes;
 using Kingmaker.Blueprints.Classes.Prerequisites;
 using Kingmaker.Blueprints.Classes.Selection;
 using Xunit;
+using static BlueprintCore.Test.TestData;
 
 namespace BlueprintCore.Test.Blueprints.Classes.Features
 {
@@ -18,6 +20,112 @@ namespace BlueprintCore.Test.Blueprints.Classes.Features
     protected override FeatureSelectionConfigurator GetConfigurator(string guid)
     {
       return FeatureSelectionConfigurator.For(guid);
+    }
+
+    [Fact]
+    public void SetIgnorePrerequisites()
+    {
+      GetConfigurator(Guid)
+          .SetIgnorePrerequisites()
+          .Configure();
+
+      var selection = BlueprintTool.Get<BlueprintFeatureSelection>(Guid);
+      Assert.True(selection.IgnorePrerequisites);
+    }
+
+    [Fact]
+    public void SetIgnorePrerequisites_False()
+    {
+      GetConfigurator(Guid)
+          .SetIgnorePrerequisites(false)
+          .Configure();
+
+      var selection = BlueprintTool.Get<BlueprintFeatureSelection>(Guid);
+      Assert.False(selection.IgnorePrerequisites);
+    }
+
+    [Fact]
+    public void SetMode()
+    {
+      GetConfigurator(Guid)
+          .SetMode(SelectionMode.OnlyRankUp)
+          .Configure();
+
+      var selection = BlueprintTool.Get<BlueprintFeatureSelection>(Guid);
+      Assert.Equal(SelectionMode.OnlyRankUp, selection.Mode);
+    }
+
+    [Fact]
+    public void SetPrimaryGroup()
+    {
+      GetConfigurator(Guid)
+          .SetPrimaryGroup(FeatureGroup.AnimalCompanion)
+          .Configure();
+
+      var selection = BlueprintTool.Get<BlueprintFeatureSelection>(Guid);
+      Assert.Equal(FeatureGroup.AnimalCompanion, selection.Group);
+    }
+
+    [Fact]
+    public void SetSecondaryGroup()
+    {
+      GetConfigurator(Guid)
+          .SetSecondaryGroup(FeatureGroup.ArcaneTricksterSpellbook)
+          .Configure();
+
+      var selection = BlueprintTool.Get<BlueprintFeatureSelection>(Guid);
+      Assert.Equal(FeatureGroup.ArcaneTricksterSpellbook, selection.Group2);
+    }
+
+    [Fact]
+    public void AddFeatures()
+    {
+      GetConfigurator(Guid)
+          .AddFeatures(FeatureGuid, ExtraFeatureGuid)
+          .Configure();
+
+      var selection = BlueprintTool.Get<BlueprintFeatureSelection>(Guid);
+
+      Assert.Equal(2, selection.m_AllFeatures.Length);
+      Assert.Contains(TestFeature.ToReference<BlueprintFeatureReference>(), selection.m_AllFeatures);
+      Assert.Contains(ExtraFeature.ToReference<BlueprintFeatureReference>(), selection.m_AllFeatures);
+    }
+
+    [Fact]
+    public void AddFeatures_WithExisting()
+    {
+      // First pass
+      GetConfigurator(Guid)
+          .AddFeatures(FeatureGuid)
+          .Configure();
+
+      GetConfigurator(Guid)
+          .AddFeatures(ExtraFeatureGuid)
+          .Configure();
+
+      var selection = BlueprintTool.Get<BlueprintFeatureSelection>(Guid);
+
+      Assert.Equal(2, selection.m_AllFeatures.Length);
+      Assert.Contains(TestFeature.ToReference<BlueprintFeatureReference>(), selection.m_AllFeatures);
+      Assert.Contains(ExtraFeature.ToReference<BlueprintFeatureReference>(), selection.m_AllFeatures);
+    }
+
+    [Fact]
+    public void RemoveFeatures()
+    {
+      // First pass
+      GetConfigurator(Guid)
+          .AddFeatures(FeatureGuid, ExtraFeatureGuid)
+          .Configure();
+
+      GetConfigurator(Guid)
+          .RemoveFeatures(FeatureGuid)
+          .Configure();
+
+      var selection = BlueprintTool.Get<BlueprintFeatureSelection>(Guid);
+
+      Assert.Single(selection.m_AllFeatures);
+      Assert.Contains(ExtraFeature.ToReference<BlueprintFeatureReference>(), selection.m_AllFeatures);
     }
 
     [Fact]
