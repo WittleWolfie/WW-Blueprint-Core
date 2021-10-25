deed597cd73a49bc8718c7bc103f3dfe6b9a2fb4
TylerGoeringer <tyler.goeringer@gmail.com>
2021-10-24 17:16:01 -0700

    Add ArchetypeConfigurator

---
+++ Tests/Blueprints/Classes/ArchetypeConfiguratorTest.cs
@@ -0,0 +1,47 @@
+using BlueprintCore.Blueprints;
+using BlueprintCore.Blueprints.Classes;
+using Kingmaker.Blueprints.Classes;
+using Kingmaker.Localization;
+using Xunit;
+
+namespace BlueprintCore.Test.Blueprints.Classes
+{
+  /**
+   * Feature specific tests should go in CommonFeatureConfiguratorTest which is shared with
+   * FeatureSelectionConfiguratorTest.
+   */
+  public class ArchetypeConfiguratorTest : BlueprintConfiguratorTest<BlueprintArchetype, ArchetypeConfigurator>
+  {
+    public ArchetypeConfiguratorTest() : base()
+    {
+      BlueprintPatch.Create<BlueprintArchetype>(Guid);
+    }
+
+    protected override ArchetypeConfigurator GetConfigurator(string guid)
+    {
+      return ArchetypeConfigurator.For(guid);
+    }
+
+    [Fact]
+    public void SetDisplayName()
+    {
+      GetConfigurator(Guid)
+          .SetDisplayName(new LocalizedString { m_Key = "name" })
+          .Configure();
+
+      var archetype = BlueprintTool.Get<BlueprintArchetype>(Guid);
+      Assert.Equal("name", archetype.LocalizedName.m_Key);
+    }
+
+    [Fact]
+    public void SetDescription()
+    {
+      GetConfigurator(Guid)
+          .SetDescription(new LocalizedString { m_Key = "description" })
+          .Configure();
+
+      var archetype = BlueprintTool.Get<BlueprintArchetype>(Guid);
+      Assert.Equal("description", archetype.LocalizedDescription.m_Key);
+    }
+  }
+}
