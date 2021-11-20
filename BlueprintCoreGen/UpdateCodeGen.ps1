18a133a04440c1752b192d52576e4bee03b25768
TylerGoeringer <tyler.goeringer@gmail.com>
2021-11-20 12:42:33 -0800

    Add one-click script to copy code-gen after running

---
+++ BlueprintCoreGen/UpdateCodeGen.ps1
@@ -0,0 +1,3 @@
+﻿copy-item .\BlueprintConfigurators\* ..\..\..\..\Blueprint-Core\BlueprintCore\Blueprints\Configurators -force -recurse -verbose
+copy-item .\ActionsBuilder\* ..\..\..\..\Blueprint-Core\BlueprintCore\Actions\Builder -force -recurse -verbose
+copy-item .\ConditionsBuilder\* ..\..\..\..\Blueprint-Core\BlueprintCore\Conditions\Builder -force -recurse -verbose
