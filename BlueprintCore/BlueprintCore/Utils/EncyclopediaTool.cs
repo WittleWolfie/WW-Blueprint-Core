5da77a8660a1b3ac81a83d0351a4eb1a074aa0bf
Aegonek <dominikjachymczyk@gmail.com>
2022-01-08 12:41:54 +0100

    Created empty EncyclopediaTool. Moved entries to json file.

---
+++ BlueprintCore/BlueprintCore/Utils/EncyclopediaTool.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BlueprintCore.Utils;
+
+namespace BlueprintCore.BlueprintCore.Utils;
+
+public static class EncyclopediaTool { }
