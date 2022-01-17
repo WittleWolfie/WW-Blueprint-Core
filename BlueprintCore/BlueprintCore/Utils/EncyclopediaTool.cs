fd841f8e801a7e217a52dd6bbaab7211f6e3ac7d
Aegonek <53911798+Aegonek@users.noreply.github.com>
2022-01-17 19:36:13 +0100

    EncyclopediaTool (#32)
    * Ran the code gen.
    
    * Reverted accicdental changes to tutorials and tests.
    
    * Created empty EncyclopediaTool. Moved entries to json file.
    
    * Moved entries to .json file.
    
    * Added test for loading encyclopedia entries from file.
    
    * Doesn't work out of the box yet. Working on it.
    
    * Got that to work!
    
    * Fixed issue with untagging.
    
    * Update ExposeInternalsToTests.cs
    
    Added a newline at the end.
    
    * Now saving entries as EmbeddedResource. Booped the version up.
    
    * Reverted auto generated changes to ActionsBuilder and ConditionsBuilder.
    
    * Removed duplicated BlueprintCore in namespaces.
    
    * Added info about original implementation
    
    * Removed old comment.
    
    * Minor tweaks.
    
    * Added doc comments.
    
    * Change to 1.3.2
    
    Consider this still a minor revision
    
    Co-authored-by: WittleWolfie <tyler.goeringer@gmail.com>

---
+++ BlueprintCore/BlueprintCore/Utils/EncyclopediaTool.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Text.Json;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using BlueprintCore.Utils;
+using BlueprintCore.Internal;
+using System.Resources;
+
+namespace BlueprintCore.Utils;
+
+/// Tool for adding and removing encyclopedia entry tags from descriptions.
+/// Based on TabletopTweaks.Utilities.DescriptionTools by Vek17 - https://github.com/Vek17/WrathMods-TabletopTweaks/
+public static class EncyclopediaTool
+{
+  private static readonly LogWrapper Logger = LogWrapper.GetInternal("EncyclopediaTool");
+
+  private static Lazy<EncyclopediaEntry[]> encyclopediaEntries = new Lazy<EncyclopediaEntry[]>(() =>
+  {
+    try
+    {
+      var assembly = typeof(EncyclopediaTool).Assembly;
+      var resourceName = "BlueprintCore.resources.encyclopedia.json";
+
+      using (Stream stream = assembly.GetManifestResourceStream(resourceName))
+      using (StreamReader reader = new StreamReader(stream))
+      {
+        return JsonSerializer.Deserialize<EncyclopediaEntry[]>(stream)!;
+      }
+    }
+    catch (FileNotFoundException ex)
+    {
+      Logger.Error($"Couldn't read file with Encyclopedia Entries! Message: {ex.Message}");
+      throw ex;
+    }
+  });
+  internal static EncyclopediaEntry[] EncyclopediaEntries => encyclopediaEntries.Value;
+
+  /// <summary>
+  /// Adds encyclopedia entry tags to <c>text</c>.
+  /// For an example, see class <c>BlueprintCore.Test.UtilsEncyclopediaToolTest</c> in <b>Blueprint-Core-Gen</b> project.
+  /// <example>
+  public static string TagEncyclopediaEntries(string text)
+  {
+    foreach (var entry in EncyclopediaEntries)
+    {
+      text = entry.TagEntry(text);
+    }
+
+    return text;
+  }
+
+  /// <summary>
+  /// Removes encyclopedia entry tags from <c>text</c>.
+  /// For an example, see class <c>BlueprintCore.Test.UtilsEncyclopediaToolTest</c> in <b>Blueprint-Core-Gen</b> project.
+  /// </summary>
+  public static string UntagEncyclopediaEntries(string text)
+  {
+    foreach (var entry in EncyclopediaEntries)
+    {
+      text = entry.UntagEntry(text);
+    }
+
+    return text;
+  }
+
+  internal class EncyclopediaEntry
+  {
+    public string Entry { get; }
+    public List<string> Patterns { get; }
+
+    public Regex EntryPattern { get; }
+    public EncyclopediaEntry(string entry, List<string> patterns)
+    {
+      Entry = entry;
+      Patterns = patterns;
+      EntryPattern = new Regex($@"{{g\|Encyclopedia:{Entry}}}(?<text>.*?){{/g}}");
+    }
+
+    public string TagEntry(string text)
+    {
+      foreach (var pattern in this.Patterns)
+      {
+        // Changed from Vek implementation because it seems that vanilla game only tags each entry once in a description
+        var matches = Regex.Matches(text, pattern, RegexOptions.IgnoreCase);
+        foreach (Match match in matches)
+        {
+          if (match.Success)
+          {
+            var context = new MatchContext(match, text);
+
+            if (!context.IsMatchStandaloneWord() || IsAlreadyTagged(context))
+            {
+              continue;
+            }
+
+            text = context.Preceding + this.WrapTextInEntryTag(match.Value) + context.Following;
+            return text; // return after tagging first entry
+          }
+        }
+      }
+
+      return text;
+    }
+
+    public string UntagEntry(string text)
+    {
+      return EntryPattern.Replace(text, m => m.Groups["text"].Value);
+    }
+
+    private string WrapTextInEntryTag(string text)
+    {
+      return $"{{g|Encyclopedia:{Entry}}}{text}{{/g}}";
+    }
+
+    public static bool IsAlreadyTagged(MatchContext context)
+    {
+      return Regex.IsMatch(context.Preceding, @"{g\|Encyclopedia:[\w_]*}?\Z") || context.Following.StartsWith("{/g}");
+    }
+  }
+}
