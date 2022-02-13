1281269d949e6f5d1312ea5e928e730bcea12ea9
TylerGoeringer <tyler.goeringer@gmail.com>
2022-02-12 16:06:07 -0800

    Remove empty arrays which are handled automatically in code-gen

--- BlueprintCore/BlueprintCore/Utils/Constants.cs
+++ BlueprintCore/BlueprintCore/Utils/Constants.cs
@@ -1,6 +1,4 @@
-using Kingmaker.Blueprints.Items.Armors;
 using Kingmaker.ElementsSystem;
-using Kingmaker.Enums;
 using Kingmaker.Localization;
 using Kingmaker.ResourceLinks;
 using Kingmaker.RuleSystem;
@@ -20,8 +18,6 @@ namespace BlueprintCore.Utils
     public static class Empty
     {
       public static readonly ActionList Actions = new() { Actions = new GameAction[0] };
-      public static readonly ArmorProficiencyGroup[] ArmorProficiencies =
-          new ArmorProficiencyGroup[0];
       public static readonly ConditionsChecker Conditions = new() { Conditions = new Condition[0] };
       public static readonly ContextDiceValue DiceValue = new()
       {
@@ -30,7 +26,6 @@ namespace BlueprintCore.Utils
         BonusValue = 0
       };
       public static readonly LocalizedString String = new();
-      public static readonly WeaponCategory[] WeaponCategories = new WeaponCategory[0];
       public static readonly PrefabLink PrefabLink = new();
     }
   }
