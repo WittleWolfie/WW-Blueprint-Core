fe988023f8c82a716f89ecc590d3f5f499ef8315
TylerGoeringer <tyler.goeringer@gmail.com>
2022-02-16 17:21:15 -0800

    Migrate type name overrides to TypeTool
    These should really just apply globally and they're much simpler to declare this way.

--- BlueprintCoreGen/CodeGen/TypeTool.cs
+++ BlueprintCoreGen/CodeGen/TypeTool.cs
@@ -11,11 +11,14 @@ namespace BlueprintCoreGen.CodeGen
   /// </summary>
   public class TypeTool
   {
+
     /// <summary>
     /// Recursive function which generates the correct type name for generic types.
     /// </summary>
     public static string GetName(Type type)
     {
+      if (TypeNameOverrides.ContainsKey(type)) { return TypeNameOverrides[type]; }
+
       if (type.HasElementType && type.BaseType == typeof(Array))
       {
         return $"{GetName(type.GetElementType())}[]";
@@ -84,6 +87,7 @@ namespace BlueprintCoreGen.CodeGen
           ?.GetGenericArguments()[0];
     }
 
+    // TODO: Delete? Seems like this isn't needed.
     private static string GetSimpleTypeName(Type type)
     {
       //if (string.IsNullOrEmpty(type.Namespace))
@@ -92,5 +96,52 @@ namespace BlueprintCoreGen.CodeGen
       //}
       return type.Name;
     }
+
+    /// <summary>
+    /// Type name overrides. Applies globally.
+    /// </summary>
+    private static Dictionary<Type, string> TypeNameOverrides =
+        new()
+        {
+          // Name conflicts
+          { typeof(Kingmaker.AI.Blueprints.TargetType), "Kingmaker.AI.Blueprints.TargetType" },
+          { typeof(Kingmaker.UnitLogic.Abilities.Components.TargetType), "Kingmaker.UnitLogic.Abilities.Components.TargetType" },
+          { typeof(Kingmaker.UnitLogic.Mechanics.ValueType), "Kingmaker.UnitLogic.Mechanics.ValueType" },
+
+          { typeof(bool), "bool" },
+          { typeof(bool?), "bool" },
+
+          { typeof(byte), "byte" },
+          { typeof(byte?), "byte" },
+
+          { typeof(sbyte), "sbyte" },
+          { typeof(sbyte?), "sbyte" },
+
+          { typeof(ushort), "ushort" },
+          { typeof(ushort?), "ushort" },
+
+          { typeof(int), "int" },
+          { typeof(int?), "int" },
+
+          { typeof(uint), "uint" },
+          { typeof(uint?), "uint" },
+
+          { typeof(long), "long" },
+          { typeof(long?), "long" },
+
+          { typeof(ulong), "ulong" },
+          { typeof(ulong?), "ulong" },
+
+          { typeof(char), "char" },
+          { typeof(char?), "char" },
+
+          { typeof(double), "double" },
+          { typeof(double?), "double" },
+
+          { typeof(float), "float" },
+          { typeof(float?), "float" },
+
+          { typeof(string), "string" },
+        };
   }
 }
