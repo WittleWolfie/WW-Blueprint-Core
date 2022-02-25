b3510e747266f044c111083198fe8aa2e3d10f62
TylerGoeringer <tyler.goeringer@gmail.com>
2022-02-25 12:50:29 -0800

    Add constants for temporary armor enhancements

---
+++ BlueprintCore/BlueprintCore/Blueprints/Enchants.cs
@@ -0,0 +1,31 @@
+﻿using BlueprintCore.Utils;
+using Kingmaker.Blueprints;
+using Kingmaker.Blueprints.Items.Ecnchantments;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlueprintCore.Blueprints
+{
+  /// <summary>
+  /// Constant references to enchantment blueprints.
+  /// </summary>
+  public static class Enchants
+  {
+    public static class Armor
+    {
+      public static readonly Blueprint<BlueprintItemEnchantment, BlueprintItemEnchantmentReference> TemporaryEnhancementBonus1 =
+        "1d9b60d57afb45c4f9bb0a3c21bb3b98";
+      public static readonly Blueprint<BlueprintItemEnchantment, BlueprintItemEnchantmentReference> TemporaryEnhancementBonus2 =
+        "d45bfd838c541bb40bde7b0bf0e1b684";
+      public static readonly Blueprint<BlueprintItemEnchantment, BlueprintItemEnchantmentReference> TemporaryEnhancementBonus3 =
+        "51c51d841e9f16046a169729c13c4d4f";
+      public static readonly Blueprint<BlueprintItemEnchantment, BlueprintItemEnchantmentReference> TemporaryEnhancementBonus4 =
+        "a23bcee56c9fcf64d863dafedb369387";
+      public static readonly Blueprint<BlueprintItemEnchantment, BlueprintItemEnchantmentReference> TemporaryEnhancementBonus5 =
+        "15d7d6cbbf56bd744b37bbf9225ea83b";
+    }
+  }
+}
