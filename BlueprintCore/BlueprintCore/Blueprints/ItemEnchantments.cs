df95b2c829a8dcbbc141e2e1725f50d95af504fa
TylerGoeringer <tyler.goeringer@gmail.com>
2022-02-27 16:24:40 -0800

    Rename Enchants > ItemEnchantments
    When / if I move to auto-generating some of these this format is going to be needed. This maps 1:1 to blueprint types.

---
+++ BlueprintCore/BlueprintCore/Blueprints/ItemEnchantments.cs
@@ -0,0 +1,18 @@
+﻿using BlueprintCore.Utils;
+using Kingmaker.Blueprints;
+using Kingmaker.Blueprints.Items.Ecnchantments;
+
+namespace BlueprintCore.Blueprints
+{
+  /// <summary>
+  /// Constant references to enchantment blueprints.
+  /// </summary>
+  public static class ItemEnchantments
+  {
+    public static readonly Blueprint<BlueprintItemEnchantment, BlueprintItemEnchantmentReference> TemporaryArmorEnhancementBonus1 = "1d9b60d57afb45c4f9bb0a3c21bb3b98";
+    public static readonly Blueprint<BlueprintItemEnchantment, BlueprintItemEnchantmentReference> TemporaryArmorEnhancementBonus2 = "d45bfd838c541bb40bde7b0bf0e1b684";
+    public static readonly Blueprint<BlueprintItemEnchantment, BlueprintItemEnchantmentReference> TemporaryArmorEnhancementBonus3 = "51c51d841e9f16046a169729c13c4d4f";
+    public static readonly Blueprint<BlueprintItemEnchantment, BlueprintItemEnchantmentReference> TemporaryArmorEnhancementBonus4 = "a23bcee56c9fcf64d863dafedb369387";
+    public static readonly Blueprint<BlueprintItemEnchantment, BlueprintItemEnchantmentReference> TemporaryArmorEnhancementBonus5 = "15d7d6cbbf56bd744b37bbf9225ea83b";
+  }
+}
