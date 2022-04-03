85c3c0f74a7830bbd5cdfeeb88f0af0e6336a2f9
TylerGoeringer <tyler.goeringer@gmail.com>
2022-04-03 12:16:58 -0700

    Add class tracking ignored types

---
+++ BlueprintCoreGen/CodeGen/Overrides/Ignored.cs
@@ -0,0 +1,34 @@
+﻿using Kingmaker.Designers.EventConditionActionSystem.Actions;
+using Kingmaker.Designers.EventConditionActionSystem.Conditions;
+using System;
+using System.Collections.Generic;
+
+namespace BlueprintCoreGen.CodeGen.Overrides.Actions
+{
+  /// <summary>
+  /// Stores types which should not be included in builders or configurators. Usually this indicates they are unused by
+  /// the game but it is also used for some manual overrides such as basic condition operations implemented directly in
+  /// ConditionsBuilder.
+  /// </summary>
+  public static class Ignored
+  {
+    public static readonly List<Type> BuilderTypes =
+      new()
+      {
+        // ** Types implemented in ActionsBuilder **//
+
+        typeof(Conditional),
+
+        // *****************************************//
+
+        // ** Types implemented in ConditionsBuilder **//
+
+        typeof(False),
+        typeof(GreaterThan),
+        typeof(LessThan),
+        typeof(OrAndLogic),
+
+        // ********************************************//
+      };
+  }
+}
