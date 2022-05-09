3f3b1fa37ebac5d960a5c9f20d0895378d0dfb8f
TylerGoeringer <tyler.goeringer@gmail.com>
2022-05-08 18:40:57 -0700

    Fix UpdateCodeGen.ps1 script to properly erase and copy

--- BlueprintCoreGen/UpdateCodeGen.ps1
+++ BlueprintCoreGen/UpdateCodeGen.ps1
@@ -1,7 +1,8 @@
-﻿copy-item .\BlueprintConfigurators\* ..\..\..\..\BlueprintCore\BlueprintCore\Blueprints\Configurators -force -recurse -verbose
+﻿remove-item ..\..\..\..\BlueprintCore\BlueprintCore\Blueprints\Configurators -force -recurse
+copy-item .\BlueprintConfigurators\ ..\..\..\..\BlueprintCore\BlueprintCore\Blueprints\Configurators -force -recurse -verbose
 
 remove-item ..\..\..\..\BlueprintCore\BlueprintCore\Actions\Builder -force -recurse -exclude ActionsBuilder.cs
-copy-item .\Actions\Builder\* ..\..\..\..\BlueprintCore\BlueprintCore\Actions\Builder -force -recurse -verbose
+copy-item .\Actions\Builder\ ..\..\..\..\BlueprintCore\BlueprintCore\Actions\Builder -force -recurse -verbose
 
 remove-item ..\..\..\..\BlueprintCore\BlueprintCore\Conditions\Builder -force -recurse -exclude ConditionsBuilder.cs
-copy-item .\Conditions\Builder\* ..\..\..\..\BlueprintCore\BlueprintCore\Conditions\Builder -force -recurse -verbose
+copy-item .\Conditions\Builder\ ..\..\..\..\BlueprintCore\BlueprintCore\Conditions\Builder -force -recurse -verbose
