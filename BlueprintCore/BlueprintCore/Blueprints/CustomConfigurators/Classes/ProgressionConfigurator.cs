782004a709ef4dac379f7ddac12605f0b80ca234
TylerGoeringer <tyler.goeringer@gmail.com>
2022-07-04 18:05:50 -0700

    Move LevelEntryBuilder to Utils for sharing with ArchetypeConfigurator #35

--- BlueprintCore/BlueprintCore/Blueprints/CustomConfigurators/Classes/ProgressionConfigurator.cs
+++ BlueprintCore/BlueprintCore/Blueprints/CustomConfigurators/Classes/ProgressionConfigurator.cs
@@ -1,5 +1,6 @@
 ﻿using BlueprintCore.Blueprints.Configurators.Classes;
 using BlueprintCore.Utils;
+using BlueprintCore.Utils.Types;
 using Kingmaker.Blueprints;
 using Kingmaker.Blueprints.Classes;
 using System.Collections.Generic;
@@ -292,41 +293,6 @@ namespace BlueprintCore.Blueprints.CustomConfigurators.Classes
     }
 
     // End Converters
-  }
-
-  /// <summary>
-  /// Builder utility for <see cref="LevelEntry"/> arrays.
-  /// </summary>
-  public class LevelEntryBuilder
-  {
-    private static readonly List<LevelEntry> LevelEntries = new();
-
-    public static LevelEntryBuilder New()
-    {
-      return new();
-    }
-
-    /// <summary>
-    /// Adds a new <see cref="LevelEntry"/> for the given level.
-    /// </summary>
-    public LevelEntryBuilder AddEntry(int level, params Blueprint<BlueprintFeatureBaseReference>[] features)
-    {
-      LevelEntries.Add(
-        new()
-        {
-          Level = level,
-          m_Features = features.Select(f => f.Reference).ToList()
-        });
-      return this;
-    }
-
-    /// <summary>
-    /// Returns a <see cref="UIGroup"/> array.
-    /// </summary>
-    public LevelEntry[] GetEntries()
-    {
-      return LevelEntries.ToArray();
-    }
   }
 
   /// <summary>
---
+++ BlueprintCore/BlueprintCore/Utils/Types/LevelEntry.cs
@@ -0,0 +1,42 @@
+﻿using Kingmaker.Blueprints.Classes;
+using Kingmaker.Blueprints;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BlueprintCore.Utils.Types
+{
+  /// <summary>
+  /// Builder utility for <see cref="LevelEntry"/> arrays.
+  /// </summary>
+  public class LevelEntryBuilder
+  {
+    private static readonly List<LevelEntry> LevelEntries = new();
+
+    public static LevelEntryBuilder New()
+    {
+      return new();
+    }
+
+    /// <summary>
+    /// Adds a new <see cref="LevelEntry"/> for the given level.
+    /// </summary>
+    public LevelEntryBuilder AddEntry(int level, params Blueprint<BlueprintFeatureBaseReference>[] features)
+    {
+      LevelEntries.Add(
+        new()
+        {
+          Level = level,
+          m_Features = features.Select(f => f.Reference).ToList()
+        });
+      return this;
+    }
+
+    /// <summary>
+    /// Returns a <see cref="UIGroup"/> array.
+    /// </summary>
+    public LevelEntry[] GetEntries()
+    {
+      return LevelEntries.ToArray();
+    }
+  }
+}
