5179e684fc4ac817f1695e5e30cd5025bb901312
TylerGoeringer <tyler.goeringer@gmail.com>
2022-07-08 17:11:35 -0700

    Create ContextValuesTest.cs

---
+++ Tests/Utils/Types/ContextValuesTest.cs
@@ -0,0 +1,77 @@
+﻿using BlueprintCore.Test.TestData;
+using BlueprintCore.Utils.Types;
+using Kingmaker.Enums;
+using Kingmaker.UnitLogic.Abilities;
+using Kingmaker.UnitLogic.Mechanics;
+using Kingmaker.UnitLogic.Mechanics.Properties;
+using Xunit;
+
+namespace BlueprintCore.Test.Utils.Types
+{
+  public class ContextValuesTest
+  {
+    [Fact]
+    public void Constant()
+    {
+      var value = ContextValues.Constant(2);
+      Assert.Equal(expected: ContextValueType.Simple, actual: value.ValueType);
+      Assert.Equal(expected: 2, actual: value.Value);
+    }
+
+    [Fact]
+    public void Rank()
+    {
+      var value = ContextValues.Rank();
+      Assert.Equal(expected: ContextValueType.Rank, actual: value.ValueType);
+      Assert.Equal(expected: AbilityRankType.Default, actual: value.ValueRank);
+    }
+
+    [Fact]
+    public void Rank_WithType()
+    {
+      var value = ContextValues.Rank(AbilityRankType.DamageDice);
+      Assert.Equal(expected: ContextValueType.Rank, actual: value.ValueType);
+      Assert.Equal(expected: AbilityRankType.DamageDice, actual: value.ValueRank);
+    }
+
+    [Fact]
+    public void Shared()
+    {
+      var value = ContextValues.Shared(AbilitySharedValue.Duration);
+      Assert.Equal(expected: ContextValueType.Shared, actual: value.ValueType);
+      Assert.Equal(expected: AbilitySharedValue.Duration, actual: value.ValueShared);
+    }
+
+    [Fact]
+    public void Property()
+    {
+      var value = ContextValues.Property(UnitProperty.CR);
+      Assert.Equal(expected: ContextValueType.TargetProperty, actual: value.ValueType);
+      Assert.Equal(expected: UnitProperty.CR, actual: value.Property);
+    }
+
+    [Fact]
+    public void Property_ToCaster()
+    {
+      var value = ContextValues.Property(UnitProperty.CR, toCaster: true);
+      Assert.Equal(expected: ContextValueType.CasterProperty, actual: value.ValueType);
+      Assert.Equal(expected: UnitProperty.CR, actual: value.Property);
+    }
+
+    [Fact]
+    public void CustomProperty()
+    {
+      var value = ContextValues.CustomProperty(Guids.Property);
+      Assert.Equal(expected: ContextValueType.TargetProperty, actual: value.ValueType);
+      Assert.Equal(expected: TestData.Blueprints.Property.Reference, actual: value.m_CustomProperty);
+    }
+
+    [Fact]
+    public void CustomProperty_ToCaster()
+    {
+      var value = ContextValues.CustomProperty(Guids.Property, toCaster: true);
+      Assert.Equal(expected: ContextValueType.CasterProperty, actual: value.ValueType);
+      Assert.Equal(expected: TestData.Blueprints.Property.Reference, actual: value.m_CustomProperty);
+    }
+  }
+}
