c00abfacff4bc1349b4b6458f9abae109d4cbeea
TylerGoeringer <tyler.goeringer@gmail.com>
2022-08-12 13:13:27 -0700

    Add Hurtful component

--- BlueprintCoreTutorial/BlueprintCoreTutorial/Solutions/Feats/HurtfulSolution.cs
+++ BlueprintCoreTutorial/BlueprintCoreTutorial/Solutions/Feats/HurtfulSolution.cs
@@ -1,21 +1,18 @@
-﻿using BlueprintCore.Blueprints.CustomConfigurators.Classes;
+﻿using BlueprintCore.Actions.Builder;
+using BlueprintCore.Actions.Builder.ContextEx;
+using BlueprintCore.Blueprints.CustomConfigurators.Classes;
 using BlueprintCore.Blueprints.References;
+using BlueprintCore.Conditions.Builder;
+using BlueprintCore.Conditions.Builder.ContextEx;
+using BlueprintCore.Conditions.Builder.NewEx;
 using BlueprintCore.Utils;
-using HarmonyLib;
 using Kingmaker.Blueprints.Classes;
 using Kingmaker.Blueprints.Classes.Selection;
-using Kingmaker.Designers;
+using Kingmaker.ElementsSystem;
 using Kingmaker.EntitySystem.Stats;
-using Kingmaker.Enums;
-using Kingmaker.PubSubSystem;
 using Kingmaker.RuleSystem.Rules;
+using Kingmaker.UnitLogic;
 using Kingmaker.UnitLogic.Mechanics.Actions;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection.Emit;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BlueprintCoreTutorial.Solutions.Feats
 {
@@ -47,7 +44,76 @@ namespace BlueprintCoreTutorial.Solutions.Feats
         .AddFeatureTagsComponent(featureTags: FeatureTag.Melee | FeatureTag.Attack | FeatureTag.Skills)
         .AddPrerequisiteStatValue(StatType.Strength, 13)
         .AddPrerequisiteFeature(FeatureRefs.PowerAttackFeature.ToString())
+        .AddComponent(
+          new HurtfulComponent(ConditionsBuilder.New().TargetInMeleeRange().HasActionsAvailable(requireSwift: true)))
         .Configure();
+    }
+
+    private class HurtfulComponent : UnitFactComponentDelegate, IDemoralizeHandler
+    {
+      private readonly ConditionsChecker Conditions;
+
+      public HurtfulComponent(ConditionsBuilder conditions)
+      {
+        Conditions = conditions.Build();
+      }
+
+      public void OnDemoralizeResolved(Demoralize demoralize, RuleSkillCheck intimidateCheck)
+      {
+        if (intimidateCheck.Success)
+        {
+          if (!Conditions.Check())
+          {
+            Logger.Verbose($"Conditions not met");
+            return;
+          }
+
+          var target = intimidateCheck.GetRuleTarget();
+          if (target is null)
+          {
+            Logger.Warn($"No target for demoralize.");
+            return;
+          }
+
+          // Make sure target is not immune to demoralize, indicated by the presence of either Buff or GreaterBuff
+          if (!target.HasFact(demoralize.Buff) || !target.HasFact(demoralize.GreaterBuff))
+          {
+            Logger.Verbose($"Target immune to demoralize");
+            return;
+          }
+
+          Logger.Verbose($"{target.CharacterName} demoralized");
+
+          var caster = Context.MaybeCaster;
+          if (caster is null)
+          {
+            Logger.Warn($"Caster is missing");
+            return;
+          }
+
+          var threatHandMelee = caster.GetThreatHandMelee();
+          if (threatHandMelee is null)
+          {
+            Logger.Warn($"Unable to make melee attack");
+            return;
+          }
+
+          var attack =
+            Context.TriggerRule<RuleAttackWithWeapon>(
+              new(caster, target, threatHandMelee.Weapon, attackBonusPenalty: 0));
+
+          if (!attack.AttackRoll.IsHit)
+          {
+            Logger.Verbose($"Attack missed, removing demoralize effects");
+            target.RemoveFact(demoralize.Buff);
+            target.RemoveFact(demoralize.GreaterBuff);
+          }
+        }
+        else
+        {
+          Logger.Verbose($"Intimidate check failed");
+        }
+      }
     }
   }
 }
