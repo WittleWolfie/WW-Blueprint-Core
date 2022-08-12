1f87bf449d7f5d709ee64f1e8469b9b9191b0a81
TylerGoeringer <tyler.goeringer@gmail.com>
2022-08-12 14:06:29 -0700

    Finish Hurtful tutorial implementation

--- BlueprintCoreTutorial/BlueprintCoreTutorial/Main.cs
+++ BlueprintCoreTutorial/BlueprintCoreTutorial/Main.cs
@@ -57,7 +57,7 @@ namespace BlueprintCoreTutorial
           MagicalAptitude.Configure();
           SkaldsVigor.Configure();
           FuriousFocus.Configure();
-          Solutions.Feats.HurtfulSolution.Configure();
+          Hurtful.Configure();
         }
         catch (Exception e)
         {
