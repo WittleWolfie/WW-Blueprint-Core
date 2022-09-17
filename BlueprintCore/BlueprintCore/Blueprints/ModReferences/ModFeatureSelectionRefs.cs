0ab2062da360538b685b592f6f4d03d9578487af
TylerGoeringer <tyler.goeringer@gmail.com>
2022-09-17 11:29:01 -0700

    Add TTT-Base Refs

--- BlueprintCore/BlueprintCore/Blueprints/ModReferences/ModFeatureSelectionRefs.cs
+++ BlueprintCore/BlueprintCore/Blueprints/ModReferences/ModFeatureSelectionRefs.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 
 namespace BlueprintCore.Blueprints.ModReferences
 {
-  internal class ModFeatureSelectionRefs
+  public class ModFeatureSelectionRefs
   {
     // **** Microscopic Content Expansion **** //
     public static readonly Blueprint<BlueprintFeatureSelectionReference> MonkCapstoneSelection = "adaa20fd-8c54-4c25-8768-053767609a1b";
@@ -22,6 +22,49 @@ namespace BlueprintCore.Blueprints.ModReferences
     public static readonly Blueprint<BlueprintFeatureSelectionReference> AntipaladinUnsanctionedKnowledgeSelection4 = "7a36edf7-0720-43d6-8cbe-ddeb0d13c6db";
     public static readonly Blueprint<BlueprintFeatureSelectionReference> TyrantDiabolicBoonSelection = "868f4200-e7e9-4aa7-8623-c42aec01a2be";
     // *************************************** //
+
+    // **** TTT-Base **** //
+    public static readonly Blueprint<BlueprintFeatureSelectionReference> AdvancedArmorTrainingSelection = "5bf17c2a-e9ee-4615-b27c-993629ed9bc3";
+    public static readonly Blueprint<BlueprintFeatureSelectionReference> AdvancedWeaponTrainingSelection = "538bc739-00c2-425b-9367-7cc02a3048b1";
+    public static readonly Blueprint<BlueprintFeatureSelectionReference> AnimalAllyFeatureSelection = "ecf97b54-4d58-4edb-8bb0-ba9e7de20751";
+    public static readonly Blueprint<BlueprintFeatureSelectionReference> ArcaneDiscoverySelection = "45b72087-ac2d-4554-aec4-f44be852eddd";
+    public static readonly Blueprint<BlueprintFeatureSelectionReference> ArmorMasterySelection = "4432c1ac-66c7-4a86-9ffb-6fa21d69232f";
+    public static readonly Blueprint<BlueprintFeatureSelectionReference> ArmorSpecializationSelection = "5bb00bcc-3dd7-46d3-a687-60157d1a9643";
+    public static readonly Blueprint<BlueprintFeatureSelectionReference> ArmorTrainingSelection = "354f1a44-26d2-4ea3-8718-905108f48e72";
+    public static readonly Blueprint<BlueprintFeatureSelectionReference> BlackBladeSelection = "44150e3f-5b9a-4976-b933-56924b1a7c76";
+    public static readonly Blueprint<BlueprintFeatureSelectionReference> BloodlineCapstoneSelection = "b66e4684-3aa7-458c-86e0-2d3e3e3054c8";
+    public static readonly Blueprint<BlueprintFeatureSelectionReference> BloodragerAberrantFeatSelection = "78ffea9a-643e-4c60-8cbb-6798c894c3b4";
+    public static readonly Blueprint<BlueprintFeatureSelectionReference> BloodragerAberrantFeatSelectionGreenrager = "5c4d1685-cae4-446f-883c-4db9a09e8d68";
+    public static readonly Blueprint<BlueprintFeatureSelectionReference> BloodragerAberrantMetamagicRagerFeatSelection = "77a6291e-de19-4b54-8055-49c6318adb65";
+    public static readonly Blueprint<BlueprintFeatureSelectionReference> BloodragerAbyssalMetamagicRagerFeatSelection = "f10277ca-b2d5-447c-b6da-496c0e8d7377";
+    public static readonly Blueprint<BlueprintFeatureSelectionReference> BloodragerArcaneMetamagicRagerFeatSelection = "28d0e4fd-1d16-478e-b6d6-e467415f79d5";
+    public static readonly Blueprint<BlueprintFeatureSelectionReference> BloodragerCelestialMetamagicRagerFeatSelection = "5625b1e1-aee5-4dd0-a8e8-0438a82f6723";
+    public static readonly Blueprint<BlueprintFeatureSelectionReference> BloodragerDestinedFeatSelection = "89b722b0-d830-4f96-82cc-ecadfa494a14";
+    public static readonly Blueprint<BlueprintFeatureSelectionReference> BloodragerDestinedFeatSelectionGreenrager = "4da50101-7fe3-4328-8ebd-250ca18495f8";
+    public static readonly Blueprint<BlueprintFeatureSelectionReference> BloodragerDestinedMetamagicRagerFeatSelection = "2ee0b476-6aba-4a7f-b23a-1aa452545aee";
+    public static readonly Blueprint<BlueprintFeatureSelectionReference> BloodragerDragonMetamagicRagerFeatSelection = "40e0698d-0af6-454c-b272-b53d8002f946";
+    public static readonly Blueprint<BlueprintFeatureSelectionReference> BloodragerElementalMetamagicRagerFeatSelection = "b88192fd-47db-4de8-95f9-0621b04a10ca";
+    public static readonly Blueprint<BlueprintFeatureSelectionReference> BloodragerFeyMetamagicRagerFeatSelection = "b3ecbc59-85d0-494c-af2d-96437e9e7373";
+    public static readonly Blueprint<BlueprintFeatureSelectionReference> BloodragerInfernalMetamagicRagerFeatSelection = "7a9a2cac-9084-4e24-9851-0af2bbbd03c6";
+    public static readonly Blueprint<BlueprintFeatureSelectionReference> BloodragerSerpentineMetamagicRagerFeatSelection = "c6963c24-0460-421d-b267-f9d193224e02";
+    public static readonly Blueprint<BlueprintFeatureSelectionReference> BloodragerUndeadMetamagicRagerFeatSelection = "6fe134ea-c475-4160-997c-4bdc934e728a";
+    public static readonly Blueprint<BlueprintFeatureSelectionReference> BroadStudySelection = "d8495546-4de0-4368-9f6a-5cbc49f503e5";
+    public static readonly Blueprint<BlueprintFeatureSelectionReference> DiverseMysteriesRevelationSelection = "f1489039-4178-40f3-bffe-5cee9a6d141b";
+    public static readonly Blueprint<BlueprintFeatureSelectionReference> DivineCommanderBattleTacticianSelection = "d0b73d22-2afb-4594-aa42-7acee3b0bd64";
+    public static readonly Blueprint<BlueprintFeatureSelectionReference> DivineCommanderCompanionSelection = "d9b99d9c-48d2-4258-94b5-65733e96c7e3";
+    public static readonly Blueprint<BlueprintFeatureSelectionReference> HammerOfGodBlessingSelection = "8528112a-8657-4661-b731-9912b97b9384";
+    public static readonly Blueprint<BlueprintFeatureSelectionReference> HuntMasterAnimalCompanionSelection = "04683492-8439-4b2c-a9e0-65297b9c3408";
+    public static readonly Blueprint<BlueprintFeatureSelectionReference> LoremasterSpellbookSelectionTTT = "af469dfb-c8c3-424a-b2eb-a33b754c3077";
+    public static readonly Blueprint<BlueprintFeatureSelectionReference> MartialWeaponProficencySelection = "9f76cf2b-b8fa-466e-a04b-7523aea99e9a";
+    public static readonly Blueprint<BlueprintFeatureSelectionReference> OppositionResearchSelection = "60fda8d3-1813-4fa9-a03a-01ea88073854";
+    public static readonly Blueprint<BlueprintFeatureSelectionReference> OracleFinalRevelationSelection = "c4952889-6788-41fd-9504-d7afe07ff494";
+    public static readonly Blueprint<BlueprintFeatureSelectionReference> PerfectBodyFlawlessMindSelection = "ac5ef6d7-a046-4c4b-9db0-196153a7978b";
+    public static readonly Blueprint<BlueprintFeatureSelectionReference> PrimalistRagePowerSelection = "6baf176b-c22c-4a41-ba42-1bce3fcd9497";
+    public static readonly Blueprint<BlueprintFeatureSelectionReference> ShieldMasterySelection = "ef38e0fe-68f1-4c88-a9de-acc421455d14";
+    public static readonly Blueprint<BlueprintFeatureSelectionReference> SorcererAberrantFeatSelection = "023785b4-51d3-4286-9be7-f520ab9805a6";
+    public static readonly Blueprint<BlueprintFeatureSelectionReference> SorcererDestinedFeatSelection = "47c5b375-38cd-419d-8417-9c2ca276dabb";
+    public static readonly Blueprint<BlueprintFeatureSelectionReference> SpellBlendingSelection = "67d5b18e-c934-4c6d-a4a6-446df6855ad6";
+    // ****************** //
 
     public static readonly List<Blueprint<BlueprintFeatureSelectionReference>> All =
       new()
@@ -43,7 +86,48 @@ namespace BlueprintCore.Blueprints.ModReferences
         TyrantDiabolicBoonSelection,
         // *************************************** //
 
-
+        // **** TTT-Base **** //
+        AdvancedArmorTrainingSelection,
+        AdvancedWeaponTrainingSelection,
+        AnimalAllyFeatureSelection,
+        ArcaneDiscoverySelection,
+        ArmorMasterySelection,
+        ArmorSpecializationSelection,
+        ArmorTrainingSelection,
+        BlackBladeSelection,
+        BloodlineCapstoneSelection,
+        BloodragerAberrantFeatSelection,
+        BloodragerAberrantFeatSelectionGreenrager,
+        BloodragerAberrantMetamagicRagerFeatSelection,
+        BloodragerAbyssalMetamagicRagerFeatSelection,
+        BloodragerArcaneMetamagicRagerFeatSelection,
+        BloodragerCelestialMetamagicRagerFeatSelection,
+        BloodragerDestinedFeatSelection,
+        BloodragerDestinedFeatSelectionGreenrager,
+        BloodragerDestinedMetamagicRagerFeatSelection,
+        BloodragerDragonMetamagicRagerFeatSelection,
+        BloodragerElementalMetamagicRagerFeatSelection,
+        BloodragerFeyMetamagicRagerFeatSelection,
+        BloodragerInfernalMetamagicRagerFeatSelection,
+        BloodragerSerpentineMetamagicRagerFeatSelection,
+        BloodragerUndeadMetamagicRagerFeatSelection,
+        BroadStudySelection,
+        DiverseMysteriesRevelationSelection,
+        DivineCommanderBattleTacticianSelection,
+        DivineCommanderCompanionSelection,
+        HammerOfGodBlessingSelection,
+        HuntMasterAnimalCompanionSelection,
+        LoremasterSpellbookSelectionTTT,
+        MartialWeaponProficencySelection,
+        OppositionResearchSelection,
+        OracleFinalRevelationSelection,
+        PerfectBodyFlawlessMindSelection,
+        PrimalistRagePowerSelection,
+        ShieldMasterySelection,
+        SorcererAberrantFeatSelection,
+        SorcererDestinedFeatSelection,
+        SpellBlendingSelection,
+        // ****************** //
       };
   }
 }
---
+++ BlueprintCore/BlueprintCore/Blueprints/ModReferences/ModSpellListRefs.cs
@@ -0,0 +1,24 @@
+﻿using BlueprintCore.Utils;
+using Kingmaker.Blueprints.Classes.Spells;
+using Kingmaker.Blueprints;
+
+namespace BlueprintCore.Blueprints.ModReferences
+{
+  public class ModSpellListRefs
+  {
+    // **** Microscopic Content Expansion **** //
+    public static readonly Blueprint<BlueprintReference<BlueprintSpellList>> AntipaladinSpelllist = "336bb6aa-f6ad-4b0c-8e0b-67ab6f55caf4";
+    public static readonly Blueprint<BlueprintReference<BlueprintSpellList>> TyrantSpelllist = "be96dde1-52c7-4f40-b0e8-cb64560c12d5";
+    // *************************************** //
+
+    // **** TTT-Base **** //
+    public static readonly Blueprint<BlueprintReference<BlueprintSpellList>> GreatKenningBloodragerSpellList = "e8081484-7ba2-9a2a-7ec1-de7a8753592d";
+    public static readonly Blueprint<BlueprintReference<BlueprintSpellList>> GreatKenningDruidSpellList = "ca102de6-cd22-d4aa-7c40-980ec9021204";
+    public static readonly Blueprint<BlueprintReference<BlueprintSpellList>> GreatKenningInquisitorSpellList = "2700da0d-d639-dc6a-1fdc-c44f5b06ebdc";
+    public static readonly Blueprint<BlueprintReference<BlueprintSpellList>> GreatKenningMagusSpellList = "3dbaaf8c-302a-8d5a-4cc1-bf0540652956";
+    public static readonly Blueprint<BlueprintReference<BlueprintSpellList>> GreatKenningPaladinSpellList = "ef93ac9c-6725-b7aa-6d2f-9f2c447528d3";
+    public static readonly Blueprint<BlueprintReference<BlueprintSpellList>> GreatKenningRangerSpellList = "593b8d53-de62-d9fa-edaa-2cd725623aab";
+    public static readonly Blueprint<BlueprintReference<BlueprintSpellList>> GreatKenningWitchSpellList = "91b5b7fc-f6c6-d2ca-6ec7-d31bb092f076";
+    // ****************** //
+  }
+}
